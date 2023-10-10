e57a7424174824f3256732bdb9e316d550874ae3
Ramandhawf <umer.usman@codistan.org>
2023-10-10 12:51:50 -0700

    index commit after AI-commit

--- index.js
+++ index.js
@@ -4,25 +4,20 @@ const env = require("dotenv");
 const express = require("express");
 const bodyParser = require("body-parser");
 var { expressjwt: jwt } = require("express-jwt");
-const cors = require('cors');
-
+const cors = require("cors");
 
 const app = express();
 // app.use(cors());
 
-app.use(cors({
-  origin: 'http://15.152.206.151'
-}));
+app.use(
+  cors({
+    origin: "http://15.152.206.151",
+  })
+);
 
-app.set('view engine', 'ejs'); // Set EJS as the view engine
-console.log(path.join(__dirname, 'views'))
-app.set('views', path.join(__dirname, 'views')); // Set the directory for views
-
-
-
-
-
-
+app.set("view engine", "ejs"); // Set EJS as the view engine
+console.log(path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views")); // Set the directory for views
 
 //Loading Routes
 const webRoutes = require("./routes/web");
@@ -32,9 +27,6 @@ const resumeRoutes = require("./routes/resume");
 const jobdetailRoutes = require("./routes/jobdetail");
 const userdataRoutes = require("./routes/userdata");
 const AiresumeRoutes = require("./routes/airoutes");
-
-
-
 
 const { sequelize } = require("./models/index");
 const errorController = require("./app/controllers/ErrorController");
@@ -50,7 +42,6 @@ app.use(
     algorithms: ["HS256"],
   }).unless({
     path: [
-
       "/api/auth/sign-up",
       "/api/auth/login",
       "/api/auth/reset-password",
@@ -70,14 +61,13 @@ app.use("/api", webRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/userdata", userdataRoutes);
-app.use("/api/resumedata",resumeRoutes );
+app.use("/api/resumedata", resumeRoutes);
 app.use("/api/jobdetail", jobdetailRoutes);
 app.use("/api/AiResume", AiresumeRoutes);
 
-
 sequelize
   //.sync({ force: true })
-   .sync({ alter: true })
+  .sync({ alter: true })
   // .sync()
   .then(() => {
     app.listen(process.env.PORT);
